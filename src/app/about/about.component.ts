78a82cbc82ca0f2e98fcb0328ed0741051be2215
LVignoni <leandrovignoni@gmail.com>
2021-05-23 20:22:20 -0300

    basics

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,14 +11,19 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const interval$ = interval(1000)  // rxjs observable / simple definition of stream / blueprint of stream
     
+    let subscription = interval$.subscribe(value => console.log("stream 1 : " + value))  //instance of stream
+    // interval$.subscribe(value => console.log("stream 2 : " + value))
+    setTimeout(() => subscription.unsubscribe(), 5000)
+
+    const click$ = fromEvent(document, "click")
+    
+    click$.subscribe(
+      event => console.log(event),
+      error => console.log(error),
+      () => console.log("completed")
+    )
   }
 
 }
-
-
-
-
-
-
-

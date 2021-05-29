fa85a5564436f0cfe2cdb8c2237a8a56c2207d9d
LVignoni <leandrovignoni@gmail.com>
2021-05-29 10:51:22 -0300

    before concatMap

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,33 +1,33 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import * as moment from 'moment';
-import {fromEvent, noop} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, tap} from 'rxjs/operators';
-import {fromPromise} from 'rxjs/internal-compatibility';
-import {Store} from '../common/store.service';
+import { fromEvent, noop } from 'rxjs';
+import { concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, tap } from 'rxjs/operators';
+import { fromPromise } from 'rxjs/internal-compatibility';
+import { Store } from '../common/store.service';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css']
 })
-export class CourseDialogComponent implements AfterViewInit {
+export class CourseDialogComponent implements AfterViewInit, OnInit {
 
     form: FormGroup;
 
-    course:Course;
+    course: Course;
 
     @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
 
-    @ViewChild('searchInput', { static: true }) searchInput : ElementRef;
+    @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course,
-        private store:Store) {
+        @Inject(MAT_DIALOG_DATA) course: Course,
+        private store: Store) {
 
         this.course = course;
 
@@ -35,9 +35,30 @@ export class CourseDialogComponent implements AfterViewInit {
             description: [course.description, Validators.required],
             category: [course.category, Validators.required],
             releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
+            longDescription: [course.longDescription, Validators.required]
         });
 
+    }
+
+    ngOnInit() {
+        this.form.valueChanges
+            .pipe(
+                filter(() => this.form.valid)
+            )
+            .subscribe(changes => {
+                const saveCourse$ = fromPromise(fetch(`/api/courses/${this.course.id}`, {
+                    method: 'PUT',
+                    body: JSON.stringify(changes),
+                    headers: {
+                        'content-type': 'application/json;'
+                    }
+                }))
+
+                saveCourse$.subscribe()
+                //combined two streams by nesting multiple subscriptions calls, something to avoid
+                //multiple request are send 
+
+            })
     }
 
     ngAfterViewInit() {

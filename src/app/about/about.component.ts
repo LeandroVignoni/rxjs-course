97629687c4bc153f7bdea1f56df9f38c9b56dc73
LVignoni <leandrovignoni@gmail.com>
2021-05-29 09:39:59 -0300

    concat

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { noop, Observable } from 'rxjs';
+import { concat, noop, Observable, of, interval } from 'rxjs';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { createHttpObservable } from "../common/util"
 import { map } from 'rxjs/operators';
@@ -13,19 +13,16 @@ export class AboutComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        // const http$ = createHttpObservable("/api/courses")
+        const neverEnd$ = interval(1000)
+        const source1$ = of(1, 2, 3) 
+        const source2$ = of(4, 5, 6)
+        
 
-        // const courses$ = http$
-        //     .pipe(
-        //         map(res => Object.values(res["payload"]))
-        //     )
+        const result$ = concat(source1$, source2$)
+        const result2$ = concat(neverEnd$ ,source1$, source2$)     // this observable never subscribes to source1 or source2 because neverEnd$ nevers get completed 
 
-        // courses$.subscribe(
-        //     courses => console.log(courses),
-        //     noop,
-        //     () => console.log("completed")
-        // )
-
+        result$.subscribe(val => console.log(val))
+        // result$.subscribe(console.log) reference
     }
 
 }
